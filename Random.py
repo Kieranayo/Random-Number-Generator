c1b11e6da8cc51c9585c94f5d1c45f64fcab0020
Kieranayo <34198176+Kieranayo@users.noreply.github.com>
2018-01-09 17:03:25 +0000

    Edit
    -_-

--- Random.py
+++ Random.py
@@ -1,18 +1,17 @@
 import random
 import time
+sc = 0
 while True:
- sc = 0
- x = random.randint(1,1)
- y = random.randint(1,1)
+ x = random.randint(1,10)
+ y = random.randint(1,10)
  num = print ("Your number: %d" % x )
  z = print ("Random number is: %d" % y )
- print (sc)
  if x == y:
     print ("Correct")
-    sc = sc + 1
+    sc += 1
  else:
     print ("Wrong")
-    sc = sc - 1
+    sc = 0
  print ("score : %d" % sc)
  time.sleep(3)
  print ("")
